6bd2c24562aea97fb49c94ef9a5263e882375597
Stuy-Kevin-Lin <klin14@stuy.edu>
2019-03-12 21:22:20 -0400

    Update

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = os.urandom(32)
 socketio = SocketIO(app)
 
 rooms = {}
+names = {}
 
 @app.route("/")
 def root():
@@ -15,15 +16,27 @@ def root():
 
 @socketio.on("joinRoom",'/')
 def joinRoom(roomInfo):
-    if request.sid in rooms:
+    if len(roomInfo) == 0:
+        return
+    if request.sid in rooms: #Checks if the user is already in a room
+        if rooms[request.sid] == roomInfo:
+            return
         leave_room(rooms[request.sid])
-    join_room(roomInfo)
-    rooms[request.sid] = roomInfo
+    join_room(roomInfo) #Places user in a room
+    rooms[request.sid] = roomInfo #Sets room of user in a dictionary for later use
     emit('joinRoom', roomInfo)
+    send("<b> " + names[request.sid] + " joined " + roomInfo + "<b>", room=roomInfo)
+
+@socketio.on('setName')
+def setname(name):
+    if len(name) == 0: #Default name
+        name = "CoolBean"
+    names[request.sid] = name
 
 @socketio.on('message')
 def message(msg):
-    send(msg, room = rooms[request.sid], broadcast = True)
+    if len(msg) != 0:
+        send("<b>" + names[request.sid] + ": </b>" + msg, room = rooms[request.sid])
 
 if __name__ == '__main__':
     socketio.run(app, debug = True)
--- static/socketstuff.js
+++ static/socketstuff.js
@@ -1,36 +1,72 @@
 var socket = io.connect('http://' + document.domain + ":" + location.port);
+var submitNameBtn = document.getElementById('newNameBtn');
+var nameinput = document.getElementById('newName');
+var msgbox = document.getElementById('msgtosend');
+var roombox = document.getElementById('roomtojoin');
 
-socket.on('connect', function() {
+socket.on('connect', function() { //Executed upon opening the site
   console.log('Successfully Connected');
   console.log('Joining General');
-  socket.emit('joinRoom', 'General');
+  socket.emit('setName', 'CoolBean'); //Default name
+  $('#nameModal').modal() //Opens name prompt
 })
 
-socket.on('joinRoom', function(msg) {
-  console.log(msg);
+socket.on('joinRoom', function(msg) { //Executed upon recieving a joinRoom event
+  console.log("Successfully joined " + msg);
 })
 
-socket.on('message', function(msg) {
+socket.on('message', function(msg) { //Executed upon recieving a message event
   var chatlog = document.getElementById('chatlog');
   var newMsg = document.createElement('li');
   var children = chatlog.children;
-  newMsg.innerHTML = msg;
-  chatlog.appendChild(newMsg)
+  if (children.length > 10) {
+    chatlog.removeChild(children[0]);
+  }
+  newMsg.innerHTML = msg; //Sets new list item to reflect the message recieved
+  chatlog.appendChild(newMsg) //Append the new element to the chat list
 })
 
-var sendMessage = function() {
+var sendMessage = function() { //Sends a message event to the server
   console.log('Sending message');
-  inputbox = document.getElementById('msgtosend');
-  var newMsg = inputbox.value;
+  var newMsg = msgbox.value;
   console.log(newMsg);
   socket.send(newMsg);
-  inputbox.value = "";
+  msgbox.value = "";
 }
 
-var joinRoom = function() {
+var joinRoom = function() { //Sends an event to the server to join a room
   console.log('Joining room');
   inputbox = document.getElementById('roomtojoin');
   var roomName = inputbox.value;
   socket.emit('joinRoom',roomName);
   inputbox = "";
 }
+
+var submitName = function() {
+  console.log('Setting name to ' + nameinput.value);
+  socket.emit('setName', nameinput.value);
+  var closebtn = document.getElementById('closemodal');
+  socket.emit('joinRoom', 'General'); //Sends the joinRoom event to the server, with the data 'General'
+  $('#nameModal').modal('hide')
+}
+
+nameinput.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    submitNameBtn.click();
+  }
+})
+
+msgbox.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    sendMessage();
+  }
+})
+
+roombox.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    joinRoom();
+  }
+})
