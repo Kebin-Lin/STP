f277adde90fccb741471c27dd5ee5e1fac91576f
Stuy-Kevin-Lin <klin14@stuy.edu>
2019-04-29 23:04:07 -0400

    Mini demo

--- refpage/app.py
+++ refpage/app.py
@@ -6,9 +6,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
 socketio = SocketIO(app)
 
+rooms = {}
+
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@socketio.on("joinRoom")
+def joinRoom(roomInfo):
+    if len(roomInfo) == 0:
+        return
+    if request.sid in rooms: #Checks if the user is already in a room
+        if rooms[request.sid] == roomInfo:
+            return
+        leave_room(rooms[request.sid])
+    join_room(roomInfo) #Places user in a room
+    rooms[request.sid] = roomInfo #Sets room of user in a dictionary for later use
+    emit('joinRoom', roomInfo)
+    send("Joined " + roomInfo, room=request.sid)
+
+@socketio.on('message')
+def message(msg):
+    if len(msg) != 0:
+        send(msg, room = rooms[request.sid])
 
 if __name__ == '__main__':
     socketio.run(app, debug = True)
--- refpage/static/socketstuff.js
+++ refpage/static/socketstuff.js
@@ -1,5 +1,53 @@
 var socket = io.connect('http://' + document.domain + ":" + location.port);
+var msgbox = document.getElementById('msgtosend');
+var roombox = document.getElementById('roomtojoin');
 
 socket.on('connect', function() { //Executed upon opening the site
   console.log('Successfully Connected');
+  socket.emit('joinRoom', 'General'); //Automatically join General
 })
+
+socket.on('message', function(msg) { //Executed upon recieving a message event
+  var chatlog = document.getElementById('chatlog');
+  var newMsg = document.createElement('li');
+  var children = chatlog.children;
+  if (children.length > 10) {
+    chatlog.removeChild(children[0]);
+  }
+  newMsg.innerHTML = msg; //Sets new list item to reflect the message recieved
+  chatlog.appendChild(newMsg) //Append the new element to the chat list
+})
+
+socket.on('joinRoom', function(msg) { //Executed upon recieving a joinRoom event
+  console.log("Successfully joined " + msg);
+})
+
+var sendMessage = function() { //Sends a message event to the server
+  console.log('Sending message');
+  var newMsg = msgbox.value;
+  console.log(newMsg);
+  socket.send(newMsg);
+  msgbox.value = "";
+}
+
+var joinRoom = function() { //Sends an event to the server to join a room
+  console.log('Joining room');
+  inputbox = document.getElementById('roomtojoin');
+  var roomName = inputbox.value;
+  socket.emit('joinRoom', roomName);
+  inputbox = "";
+}
+
+msgbox.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    sendMessage();
+  }
+});
+
+roombox.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    joinRoom();
+  }
+});
